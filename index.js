0aaf8f524f4307fe16b618da6d75fd2e435d4976
Khadejah <136977286+kkhaddejah@users.noreply.github.com>
2025-03-22 02:07:46 +0400

    updated if else statement.

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ let secondCard = 9;
 let sum = firstCard + secondCard;
 
 if (sum < 21) {
-  console.log("Do you want to draw a new card?");
+  console.log("Do you want to draw a new card? 🙂");
 } else if (sum === 21) {
-  console.log("Wohoo! You've got Blackjack!");
-} else if (sum > 21) {
-  console.log("You're out of the game!");
+  console.log("Wohoo! You've got Blackjack! 🥳");
+} else {
+  console.log("You're out of the game! 😭");
 }
