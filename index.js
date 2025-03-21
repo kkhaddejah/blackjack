b95c6c9efc0a2fbc1eacdb86a750223872f66b16
Khadejah <136977286+kkhaddejah@users.noreply.github.com>
2025-03-22 02:05:00 +0400

    initial code.

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-let firstCard = 3;
-let secondCard = 10;
-
+let firstCard = 6;
+let secondCard = 9;
 let sum = firstCard + secondCard;
 
 if (sum < 21) {
+  console.log("Do you want to draw a new card?");
+} else if (sum === 21) {
+  console.log("Wohoo! You've got Blackjack!");
+} else if (sum > 21) {
+  console.log("You're out of the game!");
 }
