e94145920cf4c254f303319fef3f5df881bee056
Khadejah <136977286+kkhaddejah@users.noreply.github.com>
2025-03-22 23:39:35 +0400

    practice(fixed).

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ let isAlive = true;
 if (sum <= 20) {
   console.log("Do you want to draw a new card? 🙂");
 } else if (sum === 21) {
-  isAlive = false;
   console.log("Wohoo! You've got Blackjack! 🥳");
   hasBlackJack = true;
 } else {
+  isAlive = false;
   console.log("You're out of the game! 😭");
 }
 
