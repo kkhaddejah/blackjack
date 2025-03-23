70f16b9af49695334dfec1e7387047d296c3e218
Khadejah <136977286+kkhaddejah@users.noreply.github.com>
2025-03-23 23:14:41 +0400

    added sum value

--- index.js
+++ index.js
@@ -4,18 +4,19 @@ let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
 let message = "";
-
 let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
 
 function startGame() {
   if (sum <= 20) {
     message = "Do you want to draw a new card?";
   } else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack!";
+    message = "You've got Blackjack!";
     hasBlackJack = true;
   } else {
     message = "You're out of the game!";
     isAlive = false;
   }
   messageEl.textContent = message;
+  sumEl.textContent = sum;
 }
