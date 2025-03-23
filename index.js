a99b92023fb84286bee612e7ea9c8b3bcc5dca20
Khadejah <136977286+kkhaddejah@users.noreply.github.com>
2025-03-23 23:37:13 +0400

    Added js and html code

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ let isAlive = true;
 let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
+let cardsEl = document.querySelector("#cards-el");
 
 function startGame() {
   if (sum <= 20) {
@@ -19,4 +20,5 @@ function startGame() {
   }
   messageEl.textContent = message;
   sumEl.textContent = "Sum: " + sum;
+  cardsEl.textContent = "Cards: 10, 4";
 }
