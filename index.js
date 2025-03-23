2b95750ad9ddeb1d1155429731679ab0c4655958
Khadejah <136977286+kkhaddejah@users.noreply.github.com>
2025-03-24 01:37:53 +0400

    Added js and html code

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let isAlive = true;
 let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
-let cardsEl = document.querySelector("#cards-el");
+let cardsEl = document.getElementById("cards-el");
 
 function startGame() {
   if (sum <= 20) {
@@ -18,7 +18,11 @@ function startGame() {
     message = "You're out of the game!";
     isAlive = false;
   }
-  messageEl.textContent = message;
+  cardsEl.textContent = "Cards: " + firstCard + " " + secondCard;
   sumEl.textContent = "Sum: " + sum;
-  cardsEl.textContent = "Cards: 10, 4";
+  messageEl.textContent = message;
+}
+
+function newCard() {
+  console.log("Drawing a new card from the deck!");
 }
